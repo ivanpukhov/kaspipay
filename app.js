5b1980cd9204b77986c60749da407ed71e85c3fb
ivaninbox <ix.pukhov@gmail.com>
2024-04-27 18:20:49 +0500

    d

---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const { exec } = require('child_process');
+const TelegramBot = require('node-telegram-bot-api');
+const Order = require('./model');
+const sequelize = require('./database');
+const {scrollAndAnalyze} = require("./check");
+
+const token = '6515597075:AAHwpV7yxqCa7ilXrP6bwOJp65xdlWCUHW4';
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+const port = 3030;
+let isProcessing = false;
+const axios = require('axios');
+
+
+
+async function initializeDatabase() {
+    await sequelize.sync();
+    console.log('База данных инициализирована');
+    processOrders();
+}
+
+initializeDatabase();
+
+const generateRandomString = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+bot.on('message', async (msg) => {
+    if (msg.chat.id === -1002037248488) {
+        const regex = /ID: (\d+)\nНомер: (\d+)\nКомментарий: (.+)\nЦена: (\d+)\nИмя клиента: (.+)\nИндекс: (.+)\nГород: (.+)\nУлица: (.+)\nДом: (.+)\nСпособ доставки: (.+)\nСпособ оплаты: (.+)/;
+        const matches = msg.text.match(regex);
+
+        if (matches) {
+            const [, id, number, comment, price, customerName, addressIndex, city, street, houseNumber, deliveryMethod, paymentMethod] = matches;
+            const fullStreet = `${city} ${street} ${houseNumber}`;
+            const fullComment = comment + generateRandomString(5);  // Предполагается, что функция generateRandomString уже определена
+
+            await Order.create({
+                serverId: id,
+                price,
+                number,
+                comment: fullComment,
+                kot: customerName,
+                user_input: addressIndex,
+                street: fullStreet,
+                deliveryMethod,
+                paymentMethod,
+                isProcessed: false,
+                isPaid: false,
+                isSend: false
+            });
+
+            console.log('Заказ сохранен в базе данных');
+        } else {
+            console.error('Сообщение не соответствует ожидаемому формату');
+        }
+    }
+});
+
+async function updateOrderStatusOnFirstServer(orderId, newStatus) {
+    const config = {
+        headers: {
+            'Authorization': `Bearer 56j48P2jHy38uvzrjtFNNkjlwdfkjbvwlkejgbUMqKhTzRjyIRj7xcmChhTYuF1VZuLLcGIsR4egG`
+        }
+    };
+
+    try {
+        const response = await axios.put(`http://localhost:3001/orders/${orderId}/status`, {
+            status: newStatus
+        }, config);
+
+        console.log(`Статус заказа ID: ${orderId} обновлен на первом сервере: ${newStatus}`);
+
+    } catch (error) {
+        console.error(`Ошибка при обновлении статуса заказа на первом сервере: ${error}`);
+    }
+}
+
+async function processOrders() {
+    if (isProcessing) return;
+    isProcessing = true;
+    await scrollAndAnalyze();
+    const orderToProcess = await Order.findOne({
+        where: {
+            isProcessed: false,
+            paymentMethod: 'kaspi'
+        }
+
+    });
+    if (orderToProcess) {
+        const commands = [
+            {cmd: `adb shell input tap 163 1083`, delay: 1500},
+            {cmd: `adb shell input swipe 300 500 300 900 200`, delay: 1000},
+            {cmd: `adb shell input tap 150 400`, delay: 1500},
+            {cmd: `adb shell input text '${orderToProcess.price}'`, delay: 1500},
+            {cmd: `adb shell input tap 50 570`, delay: 1500},
+            {cmd: `adb shell input text '${orderToProcess.number}'`, delay: 1500},
+            {cmd: `adb shell input tap 50 910`, delay: 1500},
+            {cmd: `adb shell input text '${orderToProcess.comment}'`, delay: 1500},
+            {cmd: `adb shell input swipe 300 500 300 900 200`, delay: 1000},
+            {cmd: `adb shell input tap 150 1150`, delay: 1500},
+            {cmd: `adb shell input tap 150 1010`, delay: 1500},
+            {cmd: `adb shell input tap 150 965`, delay: 1500},
+            {cmd: `adb shell input tap 150 1100`, delay: 1500},
+        ];
+
+        for (const {cmd, delay} of commands) {
+            exec(cmd, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                }
+            });
+            await new Promise(resolve => setTimeout(resolve, delay));
+        }
+
+        orderToProcess.isProcessed = true;
+        await orderToProcess.save();
+        await updateOrderStatusOnFirstServer(orderToProcess.serverId, 'Ожидает оплаты');
+        console.log(`Заказ ID: ${orderToProcess.id} обработан`);
+    }
+
+    isProcessing = false;
+    setTimeout(processOrders, 1000);
+}
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
---
+++ check.js
@@ -0,0 +1,176 @@
+const {exec} = require('child_process');
+const fs = require('fs');
+const axios = require('axios');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
+const Order = require('./model');
+
+async function adbCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Ошибка выполнения команды: ${command}`, stderr);
+                if (stderr.includes('code 137')) {
+                    console.log('Ошибка с кодом 137. Перезапуск сервера ADB...');
+                    exec('adb kill-server && adb start-server', (killError) => {
+                        if (killError) {
+                            console.error('Ошибка при перезапуске сервера ADB:', killError);
+                            reject(killError);
+                        } else {
+                            console.log('Сервер ADB успешно перезапущен.');
+                            resolve();
+                        }
+                    });
+                } else {
+                    reject(error);
+                }
+                return;
+            }
+            resolve(stdout.trim());
+        });
+    });
+}
+
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkDevice() {
+    let deviceFound = false;
+    while (!deviceFound) {
+        try {
+            const devices = await adbCommand('adb devices');
+            if (devices.includes('device')) {
+                deviceFound = true;
+                console.log('Устройство обнаружено.');
+            } else {
+                console.log('Устройство не обнаружено. Повторная попытка через 5 секунд.');
+                await delay(5000);
+            }
+        } catch (error) {
+            console.error('Ошибка при проверке устройства:', error);
+            console.log('Повторная попытка через 5 секунд.');
+            await delay(5000);
+        }
+    }
+}
+
+async function scrollToTop() {
+    console.log('Прокрутка до самого верха страницы...');
+    await checkDevice();
+    await adbCommand('adb shell input tap 281 1073');
+    for (let i = 0; i < 5; i++) {
+        await checkDevice();
+        await adbCommand('adb shell input swipe 100 700 100 1000');
+        await delay(100);
+    }
+}
+
+async function processUIDump() {
+    await checkDevice();
+    await adbCommand('adb shell uiautomator dump /sdcard/ui_dump.xml');
+    await checkDevice();
+    await adbCommand('adb pull /sdcard/ui_dump.xml ./ui_dump.xml');
+    const uiDump = fs.readFileSync('ui_dump.xml', 'utf8');
+    parser.parseString(uiDump, async (err, result) => {
+        if (err) {
+            console.error('Ошибка при разборе XML:', err);
+            return;
+        }
+        let collectedTexts = [];
+        findTexts(result.hierarchy.node, collectedTexts, 'hr.asseco.android.kaspibusiness:id/sellerComment');
+        for (const comment of collectedTexts) {
+            const order = await Order.findOne({where: {comment}});
+            if (order && !order.isPaid) {
+                const isSuccess = await updateOrderStatusOnFirstServer(order.serverId, 'Оплачено');
+                if (isSuccess) {
+                    order.isPaid = true;
+                    await order.save();
+                    console.log(`Заказ с ID ${order.serverId} обновлен как 'оплачено'`);
+                    await sendPostRequest(order);
+                } else {
+                    console.log(`Не удалось обновить статус заказа с ID ${order.serverId}`);
+                }
+            }
+        }
+    });
+}
+
+async function sendPostRequest(order) {
+    try {
+        const response = await axios.post('http://localhost:5000/process', {
+            kot: order.kot,
+            user_input: order.user_input,
+            street: order.street,
+            number: order.number,
+            serverId: order.serverId
+        });
+        console.log('POST-запрос успешно отправлен:', response.data);
+    } catch (error) {
+        console.error('Ошибка при отправке POST-запроса:', error);
+    }
+}
+
+function findTexts(node, collectedTexts, resourceId) {
+    if (node && Array.isArray(node)) {
+        node.forEach((n) => {
+            if (n.$['resource-id'] === resourceId && n.$.text) {
+                collectedTexts.push(n.$.text);
+            }
+            if (n.node) {
+                findTexts(n.node, collectedTexts, resourceId);
+            }
+        });
+    }
+}
+
+async function updateOrderStatusOnFirstServer(serverId, newStatus) {
+    try {
+        const response = await axios.put(`http://localhost:3001/orders/${serverId}/status`, {
+            status: newStatus
+        }, {
+            headers: {
+                'Authorization': `Bearer 56j48P2jHy38uvzrjtFNNkjlwdfkjbvwlkejgbUMqKhTzRjyIRj7xcmChhTYuF1VZuLLcGIsR4egG`
+            }
+        });
+        return response.status === 200;
+    } catch (error) {
+        console.error(`Ошибка при обновлении статуса заказа на первом сервере: ${error}`);
+        return false;
+    }
+}
+
+async function scrollAndAnalyze() {
+    await scrollToTop();
+    let endReached = false;
+    let previousDump = '';
+    let currentDump = '';
+    let attempts = 0;
+    while (!endReached) {
+        await processUIDump();
+        await delay(1000);
+        await delay(1000);
+        await delay(1000);
+
+        await checkDevice();
+        await adbCommand('adb shell uiautomator dump /sdcard/ui_dump.xml');
+
+        await checkDevice();
+        await adbCommand('adb pull /sdcard/ui_dump.xml');
+        currentDump = fs.readFileSync('ui_dump.xml', 'utf8');
+        if (currentDump === previousDump || attempts > 30) {
+            endReached = true;
+            console.log('Конец страницы достигнут или превышен лимит попыток.');
+        } else {
+            await checkDevice();
+            await adbCommand('adb shell input swipe 100 850 100 500');
+            previousDump = currentDump;
+            attempts++;
+        }
+    }
+}
+
+module.exports = {
+    scrollAndAnalyze
+};
